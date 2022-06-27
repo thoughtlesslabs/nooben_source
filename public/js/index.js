79c69ea43886c6a274956f64877bbff2fc5685f3
John Dietrich <john@thoughtlesslabs.com>
2022-06-27 12:10:52 -0400

    removed index.html and updated index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,52 +1,38 @@
 const express = require("express");
 const path = require("path");
-const dotenv = require("dotenv").config();
+const { searchForRepos, deleteRepo } = require("./app");
+
+const mongoose = require("mongoose");
+const repos = require("../../models/repos");
+
+mongoose.connect("mongodb://localhost:27017/nooben_source");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 const app = express();
 const port = 3000;
-
-const url = process.env.BASE_URL;
 
 app.use(express.static(path.join(__dirname, "../..", "public")));
 app.set("views", path.join(__dirname, "../..", "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  const repos = "";
-  res.render("home", { repos });
+app.get("/", async (req, res) => {
+  const allRepos = await repos.find({});
+  res.render("home", { repos: allRepos });
 });
 
-app.get("/repos", async (req, res) => {
-  const config = {
-    method: "get",
-    url: "https://api.github.com/users/thoughtlesslabs/repos",
-    // auth: {
-    //   username: "token",
-    //   password: "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",
-    // },
-    headers: {
-      Accept: "application/vnd.github.v3+json",
-      "If-None-Match":
-        'W/"92d4d07191f6cf55451f5fb93cc8368e7f97dc535ffe829681071c35bf22169e"',
-    },
-  };
+app.get("/find-repos", async (req, res) => {
+  await searchForRepos();
+  res.redirect("/");
+});
 
-  await axios(config)
-    .then(function (response) {
-      const repos = response.data;
-      // for (let repo in repos) {
-      //   const newLI = document.createElement("li");
-      //   newLI.innerText = repos[repo].name;
-      //   repoName.append(newLI);
-      // }
-      console.log(response.status);
-      console.log(response.headers);
-      return repos;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  res.render("home", { repos });
+app.get("/delete-repos", async (req, res) => {
+  await deleteRepo();
+  res.redirect("/");
 });
 
 app.listen(port, () => {
